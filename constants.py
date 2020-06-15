146ab02e4520a849c0dd6329e447ea6df182546c
Mathias Le Borgne <mathias.leborgne@gmail.com>
2020-06-15 10:05:27 +0200

    Script: increase prediction days to 15

--- constants.py
+++ constants.py
@@ -4,5 +4,5 @@ min_total_cases = 1000
 min_cases_start_date = 30
 min_days_post_peak = 8
 min_decrease_post_peak = 10.
-number_days_future_default = 10
+number_days_future_default = 15 # number of days ahead for prediction
 max_countries_display = 50  # max number of countries to display
