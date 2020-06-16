f21007258e0b8925ed4ec69cdafaffc43df8f13c
Mathias Le Borgne <mathias.leborgne@gmail.com>
2020-06-16 16:27:18 +0200

    Script: udpate min days after peak

--- constants.py
+++ constants.py
@@ -3,7 +3,8 @@
 min_new_cases = 100
 min_total_cases = 1000
 min_cases_start_date = 30
-min_days_post_peak = 8
+min_days_post_peak = 12 # minimum number of decrease days after smooth curve peak
+    # in order to actually detect "the" peak
 min_decrease_post_peak = 10.
 number_days_future_default = 15 # number of days ahead for prediction
 max_countries_display = 50  # max number of countries to display
