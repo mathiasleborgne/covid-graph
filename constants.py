0cefe1feeb4a2fa52dbe96d20bc072c35d35b2e9
Mathias Le Borgne <mathias.leborgne@gmail.com>
2020-09-06 13:34:34 +0200

    Doc

--- constants.py
+++ constants.py
@@ -8,7 +8,7 @@ min_total_cases = 1000 # minimum number of Total cases to display a country
 min_cases_start_date = 30  # minimum number of cases to start the curve display
 min_days_post_peak = 12 # minimum number of decrease days after smooth curve peak
     # in order to actually detect "the" peak
-min_decrease_post_peak = 10.
+min_decrease_post_peak = 10.  # minimum decrease un cases/deaths to detect a peak, in percents
 number_days_future_default = 15 # number of days ahead for prediction
 max_countries_display = 50  # max number of countries to display
 default_smoothing_length = 7 # default length of smoothing window, 1 week as week-ends are often counted differently
